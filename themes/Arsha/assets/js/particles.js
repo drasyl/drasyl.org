74ac00be5d48a6f5229acb10ebe3280361713604
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-12-15 11:44:54 +0100

    Geht los

---
+++ themes/Arsha/assets/js/particles.js
@@ -0,0 +1,34 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 100
+    },
+    color: {
+      value: "#ffffff"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 3,
+      random: true
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#41ead4",
+      opacity: 0.2,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false
+    }
+  },
+  retina_detect: true
+})
---
+++ themes/Arsha/postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // plugins: {
+  //   '@fullhuman/postcss-purgecss': {
+  //     content: ['themes/Arsha/layouts/**/*.html'],
+  //     whitelist: [
+  //       'highlight',
+  //       'language-bash',
+  //       'pre',
+  //       'code',
+  //       'content',
+  //       'h3',
+  //       'h4',
+  //       'ul',
+  //       'li',
+  //       'canvas'
+  //     ]
+  //   },
+  //   autoprefixer: {},
+  //   cssnano: { preset: 'default' }
+  // }
+};
