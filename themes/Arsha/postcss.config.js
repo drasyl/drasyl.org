6c347504b964a21ee665c0778503dddc76b73023
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-12-15 23:50:11 +0100

    fix postcss

--- themes/Arsha/postcss.config.js
+++ themes/Arsha/postcss.config.js
@@ -2,17 +2,9 @@ module.exports = {
   plugins: {
     '@fullhuman/postcss-purgecss': {
       content: ['themes/Arsha/layouts/**/*.html'],
-      whitelist: [
-        // 'highlight',
-        // 'language-bash',
-        // 'pre',
-        // 'code',
-        // 'content',
-        // 'h3',
-        // 'h4',
-        // 'ul',
-        // 'li',
-        // 'canvas'
+      safelist: [
+        'canvas',
+        'header-scrolled'
       ]
     },
     autoprefixer: {},
